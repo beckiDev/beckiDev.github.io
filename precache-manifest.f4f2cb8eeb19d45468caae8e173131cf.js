afdb9b95254cdb2bf3568f7ea23014a1d5904d6b
beckidev <becki@Rebeckas-MacBook-Pro.local>
2019-11-06 00:35:12 -0800

    Updates

---
+++ precache-manifest.f4f2cb8eeb19d45468caae8e173131cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001db2be0c704a65edce3f795ad4e064",
+    "url": "/beckiDev/react-app/index.html"
+  },
+  {
+    "revision": "8a41aa9a25d000c6eefb",
+    "url": "/beckiDev/react-app/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "8a9612f7db548dfdd2d8",
+    "url": "/beckiDev/react-app/static/css/main.d3e508b7.chunk.css"
+  },
+  {
+    "revision": "8a41aa9a25d000c6eefb",
+    "url": "/beckiDev/react-app/static/js/2.e2e19748.chunk.js"
+  },
+  {
+    "revision": "8a9612f7db548dfdd2d8",
+    "url": "/beckiDev/react-app/static/js/main.f7763eef.chunk.js"
+  },
+  {
+    "revision": "60da524ad8619006a36c",
+    "url": "/beckiDev/react-app/static/js/runtime-main.6e752501.js"
+  }
+]);
